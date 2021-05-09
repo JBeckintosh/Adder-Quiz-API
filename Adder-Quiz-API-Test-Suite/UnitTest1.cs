99fa205983916246157f967f8c7751f375855984
JBeckintosh <James-beck@live.com>
2021-05-09 17:09:20 +1200

    API template and Test Suite Created

---
+++ Adder-Quiz-API-Test-Suite/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adder_Quiz_API_Test_Suite
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
